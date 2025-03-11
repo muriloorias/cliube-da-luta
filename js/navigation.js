58439f122a26616aa30c542435991a186e4553a0
muriloorias <mu.a.o@outlook.com>
2025-03-11 17:51:38 -0300

    adicionando paginas

---
+++ js/navigation.js
@@ -0,0 +1,9 @@
+function goToClub() {
+      window.location.href = "club.html"
+}
+function goToMirinha(){
+      window.location.href = "spiderman.html"
+}
+
+document.getElementById("item1").addEventListener("click", goToClub);
+document.getElementById("item2").addEventListener("click", goToMirinha);
